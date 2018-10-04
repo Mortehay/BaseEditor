bf9b0137c664aabd8d56ce44e2ef971ae8b5ad65
dev3 <y.shpylovyi@ielpe.com>
2018-10-04 16:24:20 +0300

    #first try of paperjs

--- resources/assets/js/components/DisplayCurve.vue
+++ resources/assets/js/components/DisplayCurve.vue
@@ -13,9 +13,7 @@
                     <div class="card-body">
                         I'm an example component.
                     </div>
-                    <canvas id="myCanvas" ref="myCanvas" resize>
-
-                    </canvas>
+                    <canvas id="myCanvas" style="width:400px; height:400px" resize></canvas>
                 </div>
             </div>
         </div>
@@ -35,39 +33,82 @@
         },
         mounted() {
             console.log('Component mounted.')
-            /*this.$nextTick( function (){
-
-            })*/
-            //this.drawWindowId = this.$refs['myCanvas'];
         },
         created(){
             console.log(this.displayState);
-
+            setTimeout(this.toggleCurveWindow, 500);
             console.log(this.displayState);
-            setTimeout(this.toggleCurveWindow, 3000);
+
         },
         methods:{
             toggleCurveWindow: function(){
                 this.displayState = true;
                 this.drawWindow();
             },
-            drawWindow: function(){
-                //console.log(paper);
-                //paper.install(window);
-                $(function() {
-                    //console.log(this.drawWindowId);
-                    var canvas = document.getElementById('myCanvas');
-                    //console.log(canvas);
-                    paper.setup(canvas);
-                    var path = new paper.Path();
-                    path.strokeColor = 'black';
-                    var start = new paper.Point(100, 100);
-                    path.moveTo(start);
-                    path.lineTo(start.add([ 200, -50 ]));
-                    paper.view.draw();
+            drawWindoeBorder: function(){
+                $(function(){
+                    $('#myCanvas').css({'border': '1px solid black', 'margin':'0 auto'});
                 });
+            },
+            drawWindow: function(){
+                paper.install(window);
+                var __this = this;
+                var paperSetup = paper.setup(document.getElementById('myCanvas'));
+                $(function(){
 
+                    var tool = new Tool();
+                    var path;
+
+                    tool.onMouseDown = function(event) {
+                        path = new Path();
+                        path.strokeColor = 'blue';
+                        path.add(event.point);
+                    }
+
+                    tool.onMouseDrag = function(event) {
+                        path.add(event.point);
+                    }
+                    __this.drawPoligon(paperSetup, tool);
+                });
+            },
+            drawPoligon: function(paperSetup, tool){
+                $(function(){
+                    var myCanvasSize = {height:400, width:400};
+                    paper.install(window);
+                    paper.setup(document.getElementById('myCanvas'));
+                    var  decagonPosition = {xPath: 200, yPath: 70};
+                    var decagon = new Path.RegularPolygon(new Point(decagonPosition.xPath, decagonPosition.yPath), 10, 50);
+                    decagon.fillColor = '#e9e9ff';
+                    decagon.selected = true;
+
+                    view.onFrame = function(event) {
+                        // Each frame, rotate the path by 3 degrees:
+                        decagon.rotate(3);paper.view.draw();
+                        decagon.fillColor.hue += 1;
+                    }
+                    tool.onKeyDown = function(event, paper) {
+                        if (event.key == 'd' && decagon.position.x < myCanvasSize.width ) {
+                            decagon.position.x += 5;
+                            return false;
+                        }
+                        if (event.key == 'a' && decagon.position.x > 0) {
+                            decagon.position.x -= 5;
+                            return false;
+                        }
+                        if (event.key == 'w' && decagon.position.y > 0) {
+                            decagon.position.y -= 5;
+                            return false;
+                        }
+                        if (event.key == 's'  && decagon.position.y< myCanvasSize.height) {
+                            decagon.position.y += 5;
+                            return false;
+                        }
+                    }
+                    return decagon;
+
+                });
             }
+
         }
     }
 </script>
